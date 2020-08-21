56eb65c9710a10c5631987dc6197367cf9c3a812
Diego Souza <dee-souza@hotmail.com>
2020-08-21 18:29:18 -0300

    :construction: Create links and social icons

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://www.github.com/deesouza'
+    },
+    {
+        label: 'Instagram',
+        url: 'https://www.instagram.com/_deesouza_'
+    },
+    {
+        label: 'Dev.To',
+        url: 'https://dev.to/deesouza'
+    },
+    {
+        label: 'Medium',
+        url: 'https://medium.com/@diegoalves_37748'
+    },
+    {
+        label: 'LinkedIn',
+        url: 'https://www.linkedin.com.br/in/deesouza'
+    }
+];
+
+export default links;
---
+++ src/components/SocialLinks/icons.js
@@ -0,0 +1,10 @@
+import { Github, Linkedin, Instagram, Medium } from "styled-icons/boxicons-logos";
+
+const Icons = {
+    Github, 
+    Linkedin, 
+    Instagram, 
+    Medium
+}
+
+export default Icons;
