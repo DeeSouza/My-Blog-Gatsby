8b47df006bfb98d8a3f11cec5b1f7badade8ce89
Diego Souza <dee-souza@hotmail.com>
2020-08-25 10:06:41 -0300

    :construction: Create post item in home page

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as S from './styled';
 
-const PostItem = () => (
-    <S.PostItemLink to="/slug">
+const PostItem = ({ slug, category, background, title, date, description, timeToRead }) => (
+    <S.PostItemLink to={slug}>
         <S.PostItemWrapper>
-            <S.PostItemTag background="#47650b">
+            <S.PostItemTag background={background}>
                 Misc
             </S.PostItemTag>
             <S.PostItemInfo>
                 <S.PostItemDate>
-                    30 de Junho de 2020 * 4 min de leitura
+                    {date} * {timeToRead} min de leitura
                 </S.PostItemDate>
-                <S.PostItemTitle>Diga não as drogas</S.PostItemTitle>
-                <S.PostItemDescription>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores totam incidunt fugit quidem, ullam maiores qui rem hic expedita ex quisquam quas sed nesciunt distinctio, dolor, placeat itaque aliquid perspiciatis.</S.PostItemDescription>
+                <S.PostItemTitle>{title}</S.PostItemTitle>
+                <S.PostItemDescription>{description}</S.PostItemDescription>
             </S.PostItemInfo>
         </S.PostItemWrapper>
     </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+    slug: PropTypes.string.isRequired,
+    background: PropTypes.string,
+    category: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    timeToRead: PropTypes.string.isRequired,
+}
 
 export default PostItem;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,16 @@ import PostItem from '../components/PostItem';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+
+    <PostItem 
+      slug="/about/"
+      category="Misc"
+      background="red"
+      title="Teste"
+      date="20 de Agosto de 2020"
+      description="Lorem Ipsum Dolor"
+      timeToRead="30" 
+    />
   </Layout>
 )
 
